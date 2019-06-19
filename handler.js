c684b073a36b07f792a5fe7414486e5912a74df0
Tyler Long <tyler4long@gmail.com>
2019-06-18 17:39:38 -0700

    Add code for database

--- handler.js
+++ handler.js
@@ -1,8 +1,16 @@
+const { Client } = require('pg')
+
 module.exports.hello = async (event) => {
+  const client = new Client()
+  await client.connect()
+
+  const res = await client.query('SELECT NOW()')
+  await client.end()
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: `Database time is ${res.rows[0]}`,
       input: event
     }, null, 2)
   }
