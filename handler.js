a2631b36805360f5f254215dec1229abc6b1fd95
Tyler Long <tyler4long@gmail.com>
2019-07-05 11:29:10 -0700

    Try express

--- handler.js
+++ handler.js
@@ -1,16 +1,36 @@
 const { Client } = require('pg')
+const serverlessHTTP = require('serverless-http')
+const express = require('express')
 
-module.exports.hello = async (event) => {
+const app = express()
+
+app.get('/hello', async (req, res) => {
   const client = new Client(`postgresql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_URL}/dbname`)
   await client.connect()
 
-  const res = await client.query('SELECT NOW()')
+  const r = await client.query('SELECT NOW()')
   await client.end()
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Database time is ${res.rows[0].now}`
-    }, null, 2)
-  }
-}
+  res.json(r.rows[0].now)
+})
+
+app.get('/world', async (req, res) => {
+  res.send('888')
+})
+
+module.exports.app = serverlessHTTP(app)
+
+// module.exports.hello = async (event) => {
+//   const client = new Client(`postgresql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_URL}/dbname`)
+//   await client.connect()
+
+//   const res = await client.query('SELECT NOW()')
+//   await client.end()
+
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       message: `Database time is ${res.rows[0].now}`
+//     }, null, 2)
+//   }
+// }
